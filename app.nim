d57c309f6a8c8edca8eaa00324ba3f49dc600905
levshx <pavellevshic@gmail.com>
2023-02-12 13:46:49 +0300

    migrate to prologue

---
+++ app.nim
@@ -0,0 +1,28 @@
+import 
+  prologue,
+  prologue/middlewares/signedcookiesession,
+  prologue/middlewares/staticfile
+
+import 
+  urls,
+  initdb
+
+
+initDb()
+
+
+let
+  env = loadPrologueEnv(".env")
+  settings = newSettings(appName = env.getOrDefault("appName", "Prologue"),
+      debug = env.getOrDefault("debug", true),
+      port = Port(env.getOrDefault("port", 5555)),
+      secretKey = env.getOrDefault("secretKey", "")
+  )
+
+var app = newApp(settings = settings)
+
+app.use(staticFileMiddleware(env.get("staticDir")), sessionMiddleware(settings, path = "/"))
+app.addRoute(urls.indexPatterns, "/")
+app.addRoute(urls.authPatterns, "/auth")
+app.addRoute(urls.blogPatterns, "/blog")
+app.run()
